6fca377c7762be41aede2f0acbe31498a9ff80e4
John Omar D. Esguerra <esguerrajohnomar@gmail.com>
2018-05-04 09:23:34 -0800

    First commit

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,405 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmCalculator))
+        Me.btnIntegerDiv = New System.Windows.Forms.Button
+        Me.btnDivide = New System.Windows.Forms.Button
+        Me.btnMultiply = New System.Windows.Forms.Button
+        Me.btnMod = New System.Windows.Forms.Button
+        Me.btnSubtract = New System.Windows.Forms.Button
+        Me.btnAdd = New System.Windows.Forms.Button
+        Me.btnEquals = New System.Windows.Forms.Button
+        Me.btn8 = New System.Windows.Forms.Button
+        Me.btn5 = New System.Windows.Forms.Button
+        Me.btn2 = New System.Windows.Forms.Button
+        Me.btn9 = New System.Windows.Forms.Button
+        Me.btn7 = New System.Windows.Forms.Button
+        Me.btn4 = New System.Windows.Forms.Button
+        Me.btn1 = New System.Windows.Forms.Button
+        Me.btn6 = New System.Windows.Forms.Button
+        Me.btn3 = New System.Windows.Forms.Button
+        Me.btn0 = New System.Windows.Forms.Button
+        Me.btnClear = New System.Windows.Forms.Button
+        Me.btnPeriod = New System.Windows.Forms.Button
+        Me.btnSquare = New System.Windows.Forms.Button
+        Me.btnSquareBy2 = New System.Windows.Forms.Button
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.txtNum = New System.Windows.Forms.TextBox
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnIntegerDiv
+        '
+        Me.btnIntegerDiv.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnIntegerDiv.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnIntegerDiv.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIntegerDiv.Location = New System.Drawing.Point(245, 210)
+        Me.btnIntegerDiv.Name = "btnIntegerDiv"
+        Me.btnIntegerDiv.Size = New System.Drawing.Size(49, 43)
+        Me.btnIntegerDiv.TabIndex = 39
+        Me.btnIntegerDiv.Text = "\"
+        Me.btnIntegerDiv.UseVisualStyleBackColor = False
+        '
+        'btnDivide
+        '
+        Me.btnDivide.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnDivide.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnDivide.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDivide.Location = New System.Drawing.Point(245, 161)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(49, 43)
+        Me.btnDivide.TabIndex = 38
+        Me.btnDivide.Text = "/"
+        Me.btnDivide.UseVisualStyleBackColor = False
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnMultiply.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMultiply.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMultiply.Location = New System.Drawing.Point(245, 112)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(49, 43)
+        Me.btnMultiply.TabIndex = 37
+        Me.btnMultiply.Text = "*"
+        Me.btnMultiply.UseVisualStyleBackColor = False
+        '
+        'btnMod
+        '
+        Me.btnMod.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnMod.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnMod.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMod.Location = New System.Drawing.Point(190, 210)
+        Me.btnMod.Name = "btnMod"
+        Me.btnMod.Size = New System.Drawing.Size(49, 43)
+        Me.btnMod.TabIndex = 36
+        Me.btnMod.Text = "Mod"
+        Me.btnMod.UseVisualStyleBackColor = False
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnSubtract.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSubtract.Font = New System.Drawing.Font("Engravers MT", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSubtract.Location = New System.Drawing.Point(190, 161)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(49, 43)
+        Me.btnSubtract.TabIndex = 35
+        Me.btnSubtract.Text = "-"
+        Me.btnSubtract.UseVisualStyleBackColor = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAdd.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(190, 112)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(49, 43)
+        Me.btnAdd.TabIndex = 34
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnEquals
+        '
+        Me.btnEquals.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnEquals.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEquals.Font = New System.Drawing.Font("Arial", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEquals.Location = New System.Drawing.Point(20, 308)
+        Me.btnEquals.Name = "btnEquals"
+        Me.btnEquals.Size = New System.Drawing.Size(274, 43)
+        Me.btnEquals.TabIndex = 33
+        Me.btnEquals.Text = "="
+        Me.btnEquals.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn8.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(74, 112)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(49, 43)
+        Me.btn8.TabIndex = 32
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn5.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(74, 161)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(49, 43)
+        Me.btn5.TabIndex = 31
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn2.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(74, 210)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(49, 43)
+        Me.btn2.TabIndex = 30
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn9.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(129, 112)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(49, 43)
+        Me.btn9.TabIndex = 29
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn7.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(19, 112)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(49, 43)
+        Me.btn7.TabIndex = 28
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn4.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(19, 161)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(49, 43)
+        Me.btn4.TabIndex = 27
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn1.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(19, 210)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(49, 43)
+        Me.btn1.TabIndex = 26
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn6.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(129, 161)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(49, 43)
+        Me.btn6.TabIndex = 25
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn3.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(129, 210)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(49, 43)
+        Me.btn3.TabIndex = 24
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.Color.Gainsboro
+        Me.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn0.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.Location = New System.Drawing.Point(19, 259)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(49, 43)
+        Me.btn0.TabIndex = 23
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnClear.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(129, 259)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(49, 43)
+        Me.btnClear.TabIndex = 22
+        Me.btnClear.Text = "C"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnPeriod
+        '
+        Me.btnPeriod.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnPeriod.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnPeriod.Font = New System.Drawing.Font("Century Gothic", 21.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPeriod.Location = New System.Drawing.Point(74, 259)
+        Me.btnPeriod.Name = "btnPeriod"
+        Me.btnPeriod.Size = New System.Drawing.Size(49, 43)
+        Me.btnPeriod.TabIndex = 41
+        Me.btnPeriod.Text = "."
+        Me.btnPeriod.UseVisualStyleBackColor = False
+        '
+        'btnSquare
+        '
+        Me.btnSquare.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnSquare.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSquare.Font = New System.Drawing.Font("Century Gothic", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSquare.Location = New System.Drawing.Point(190, 259)
+        Me.btnSquare.Name = "btnSquare"
+        Me.btnSquare.Size = New System.Drawing.Size(49, 43)
+        Me.btnSquare.TabIndex = 42
+        Me.btnSquare.Text = "^"
+        Me.btnSquare.UseVisualStyleBackColor = False
+        '
+        'btnSquareBy2
+        '
+        Me.btnSquareBy2.BackColor = System.Drawing.Color.Gainsboro
+        Me.btnSquareBy2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSquareBy2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSquareBy2.Location = New System.Drawing.Point(245, 259)
+        Me.btnSquareBy2.Name = "btnSquareBy2"
+        Me.btnSquareBy2.Size = New System.Drawing.Size(49, 43)
+        Me.btnSquareBy2.TabIndex = 43
+        Me.btnSquareBy2.Text = "x^2"
+        Me.btnSquareBy2.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.txtNum)
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(292, 94)
+        Me.GroupBox1.TabIndex = 44
+        Me.GroupBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(8, 16)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(274, 29)
+        Me.TextBox1.TabIndex = 42
+        '
+        'txtNum
+        '
+        Me.txtNum.BackColor = System.Drawing.Color.White
+        Me.txtNum.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtNum.Enabled = False
+        Me.txtNum.Font = New System.Drawing.Font("Century Gothic", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNum.Location = New System.Drawing.Point(8, 44)
+        Me.txtNum.Multiline = True
+        Me.txtNum.Name = "txtNum"
+        Me.txtNum.Size = New System.Drawing.Size(274, 40)
+        Me.txtNum.TabIndex = 41
+        Me.txtNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'frmCalculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(315, 369)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnSquareBy2)
+        Me.Controls.Add(Me.btnSquare)
+        Me.Controls.Add(Me.btnPeriod)
+        Me.Controls.Add(Me.btnIntegerDiv)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnMod)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnEquals)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnClear)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmCalculator"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnIntegerDiv As System.Windows.Forms.Button
+    Friend WithEvents btnDivide As System.Windows.Forms.Button
+    Friend WithEvents btnMultiply As System.Windows.Forms.Button
+    Friend WithEvents btnMod As System.Windows.Forms.Button
+    Friend WithEvents btnSubtract As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnEquals As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn0 As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnPeriod As System.Windows.Forms.Button
+    Friend WithEvents btnSquare As System.Windows.Forms.Button
+    Friend WithEvents btnSquareBy2 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtNum As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,162 @@
+﻿Public Class frmCalculator
+    Dim iFirstNumber As Double
+    Dim sOperator As String
+
+    Private Sub btn0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn0.Click
+        txtNum.Text += "0"
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        txtNum.Text = ""
+        iFirstNumber = 0
+    End Sub
+
+    Private Sub btn1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn1.Click
+        txtNum.Text += "1"
+    End Sub
+
+    Private Sub btn2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn2.Click
+        txtNum.Text += "2"
+
+    End Sub
+
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+        iFirstNumber = Val(txtNum.Text)
+        sOperator = "+"
+        txtNum.Text = ""
+    End Sub
+
+    Private Sub btn3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn3.Click
+        txtNum.Text += "3"
+    End Sub
+
+    Private Sub btn4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn4.Click
+        txtNum.Text += "4"
+    End Sub
+    Private Sub btnPeriod_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPeriod.Click
+        txtNum.Text += "."
+    End Sub
+
+    Private Sub btn5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn5.Click
+        txtNum.Text += "5"
+    End Sub
+
+    Private Sub btn6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn6.Click
+        txtNum.Text += "6"
+    End Sub
+
+    Private Sub btn7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn7.Click
+        txtNum.Text += "7"
+    End Sub
+
+    Private Sub btn8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn8.Click
+        txtNum.Text += "8"
+    End Sub
+
+    Private Sub btn9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn9.Click
+        txtNum.Text += "9"
+    End Sub
+
+    Private Sub btnSubtract_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSubtract.Click
+        iFirstNumber = Val(txtNum.Text)
+        sOperator = "-"
+        txtNum.Text = ""
+    End Sub
+
+    Private Sub btnMultiply_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMultiply.Click
+        iFirstNumber = Val(txtNum.Text)
+        sOperator = "*"
+        txtNum.Text = ""
+    End Sub
+
+    Private Sub btnDivide_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnDivide.Click
+        iFirstNumber = Val(txtNum.Text)
+        sOperator = "/"
+        txtNum.Text = ""
+    End Sub
+
+    Private Sub btnMod_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMod.Click
+        iFirstNumber = Val(txtNum.Text)
+        sOperator = "%"
+        txtNum.Text = ""
+    End Sub
+
+    Private Sub btnIntegerDiv_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnIntegerDiv.Click
+        iFirstNumber = Val(txtNum.Text)
+        sOperator = "\"
+        txtNum.Text = ""
+    End Sub
+    Private Sub btnSquare_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSquare.Click
+        iFirstNumber = Val(txtNum.Text)
+        sOperator = "^"
+        txtNum.Text = ""
+    End Sub
+
+    Private Sub btnEquals_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEquals.Click
+        btn0.Enabled = True
+        btn1.Enabled = True
+        btn2.Enabled = True
+        btn3.Enabled = True
+        btn4.Enabled = True
+        btn5.Enabled = True
+        btn6.Enabled = True
+        btn7.Enabled = True
+        btn8.Enabled = True
+        btn9.Enabled = True
+        btnAdd.Enabled = True
+        btnSquare.Enabled = True
+        btnMod.Enabled = True
+        btnSubtract.Enabled = True
+        btnDivide.Enabled = True
+        btnMultiply.Enabled = True
+        btnSquareBy2.Enabled = True
+        btnIntegerDiv.Enabled = True
+        btnPeriod.Enabled = True
+        btnClear.Enabled = True
+
+        Select Case sOperator
+            Case "+"
+                txtNum.Text = iFirstNumber + Val(txtNum.Text)
+            Case "-"
+                txtNum.Text = iFirstNumber - Val(txtNum.Text)
+            Case "*"
+                txtNum.Text = iFirstNumber * Val(txtNum.Text)
+            Case "%"
+                txtNum.Text = iFirstNumber Mod Val(txtNum.Text)
+            Case "/"
+                txtNum.Text = iFirstNumber / Val(txtNum.Text)
+            Case "\"
+                txtNum.Text = iFirstNumber \ Val(txtNum.Text)
+            Case "^"
+                txtNum.Text = iFirstNumber ^ Val(txtNum.Text)
+            Case "x^2"
+                txtNum.Text = iFirstNumber ^ 2
+        End Select
+    End Sub
+
+    Private Sub btnSquareBy2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSquareBy2.Click
+        iFirstNumber = Val(txtNum.Text)
+        sOperator = "x^2"
+        txtNum.Text += "^2"
+        btn0.Enabled = False
+        btn1.Enabled = False
+        btn2.Enabled = False
+        btn3.Enabled = False
+        btn4.Enabled = False
+        btn5.Enabled = False
+        btn6.Enabled = False
+        btn7.Enabled = False
+        btn8.Enabled = False
+        btn9.Enabled = False
+        btnAdd.Enabled = False
+        btnSquare.Enabled = False
+        btnMod.Enabled = False
+        btnSubtract.Enabled = False
+        btnDivide.Enabled = False
+        btnMultiply.Enabled = False
+        btnSquareBy2.Enabled = False
+        btnIntegerDiv.Enabled = False
+        btnPeriod.Enabled = False
+        btnClear.Enabled = False
+    End Sub
+End Class
